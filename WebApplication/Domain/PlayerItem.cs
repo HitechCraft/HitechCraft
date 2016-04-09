8a671e8bd54134d595011debd5db6395207fc9bc
HitechCraft <hitechcraft.ru@gmail.com>
2016-04-09 14:07:36 +0300

    Upd playeritem user relation

--- WebApplication/Domain/PlayerItem.cs
+++ WebApplication/Domain/PlayerItem.cs
@@ -10,11 +10,16 @@
         
         public string nickname { get; set; }
 
+        [ForeignKey("User")]
+        public string user_id { get; set; }
+
         [ForeignKey("Item")]
         public int item_id { get; set; }
 
         public int item_amount { get; set; }
         
         public virtual ShopItem Item { get; set; }
+
+        public virtual ApplicationUser User { get; set; }
     }
 }
