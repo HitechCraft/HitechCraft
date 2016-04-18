0efdecb891d1eaed1998b0d96374e1d740e56648
Nikita Davidenko <nickita.davidenko@gmail.com>
2016-04-18 17:24:27 +0300

    flow: Created branch 'feature/mcshop'.

---
+++ WebApplication/Domain/ShopItem.cs
@@ -0,0 +1,21 @@
+﻿namespace WebApplication.Domain
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.Web.Mvc;
+
+    [Bind(Include = "Id, GameId, Name, Description, Amount")]
+    public class ShopItem
+    {
+        [Key]
+        public string GameId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public byte[] Image { get; set; }
+
+        //todo сделать возможность установки цен по разным видам валюты
+        public float Price { get; set; }
+    }
+}
