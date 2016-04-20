a31a2382133cea11abf9541dd0397aea88d57fa2
HitechCraft <hitechcraft.ru@gmail.com>
2016-04-20 23:59:33 +0300

    Add banlist filters

--- WebApplication/Controllers/BanSystemController.cs
+++ WebApplication/Controllers/BanSystemController.cs
@@ -17,12 +17,23 @@ namespace WebApplication.Controllers
     {
         public ActionResult Index()
         {
+            ViewBag.Player = context.Users.Select(u => new SelectListItem
+            {
+                Text = u.UserName,
+                Value = u.Id
+            });
+            
+            return View();
+        }
+
+        public ActionResult BanListPartial(int? banType, string playerId = "")
+        {
             var bans = context.Bans.ToList();
 
             Mapper.CreateMap<Ban, BanViewModel>()
                     .ForMember(dst => dst.Id, exp => exp.MapFrom(src => src.id))
                     .ForMember(dst => dst.PlayerName, exp => exp.MapFrom(src => src.name))
-                    .ForMember(dst => dst.ActionTime, exp => exp.MapFrom(src => 
+                    .ForMember(dst => dst.ActionTime, exp => exp.MapFrom(src =>
                     new DateTime(1970, 1, 1, 0, 0, 0, 0).AddSeconds(src.time).ToLocalTime()))
                     .ForMember(dst => dst.BannedBy, exp => exp.MapFrom(src => src.admin))
                     .ForMember(dst => dst.Reason, exp => exp.MapFrom(src => src.reason))
@@ -32,7 +43,19 @@ namespace WebApplication.Controllers
 
             IEnumerable<BanViewModel> banLogs = Mapper.Map<IEnumerable<Ban>, IEnumerable<BanViewModel>>(bans);
 
-            return View(banLogs);
+            //фильтрация записей
+
+            if (banType != null)
+            {
+                banLogs = banLogs.Where(bl => bl.Type == (BanType)banType);
+            }
+
+            if (playerId != String.Empty)
+            {
+                banLogs = banLogs.Where(bl => bl.PlayerName == context.Users.First(u => u.Id == playerId).UserName);
+            }
+
+            return PartialView(banLogs);
         }
 
         public ActionResult BanAction(int actionId)
