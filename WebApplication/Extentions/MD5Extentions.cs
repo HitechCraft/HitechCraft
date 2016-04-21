53a34ec3882feb1d01f979c09eec5589a4b3bf19
HitechCraft <hitechcraft.ru@gmail.com>
2016-04-21 19:49:48 +0300

    Ref replace Resources

---
+++ WebApplication/Extentions/MD5Extentions.cs
@@ -0,0 +1,45 @@
+﻿namespace WebApplication
+{
+    using System;
+    using System.Security.Cryptography;
+    using System.Text;
+
+    /// <summary>
+    /// From https://msdn.microsoft.com/ru-ru/library/system.security.cryptography.md5(v=vs.110).aspx
+    /// </summary>
+    public static class MD5Extentions
+    {
+        public static string GetMd5Hash(MD5 md5Hash, string input)
+        {
+
+            // Convert the input string to a byte array and compute the hash.
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            // Create a new Stringbuilder to collect the bytes
+            // and create a string.
+            StringBuilder sBuilder = new StringBuilder();
+
+            // Loop through each byte of the hashed data 
+            // and format each one as a hexadecimal string.
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            // Return the hexadecimal string.
+            return sBuilder.ToString();
+        }
+
+        // Verify a hash against a string.
+        public static bool VerifyMd5Hash(MD5 md5Hash, string input, string hash)
+        {
+            // Hash the input.
+            string hashOfInput = GetMd5Hash(md5Hash, input);
+
+            // Create a StringComparer an compare the hashes.
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+
+            return comparer.Compare(hashOfInput, hash) == 0;
+        }
+    }
+}
