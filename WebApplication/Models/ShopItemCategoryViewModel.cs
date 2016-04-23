ae70f7cbde15fe2eddf2694b19e3489f90010873
Nikita Davidenko <nickita.davidenko@gmail.com>
2016-04-23 17:42:49 +0300

    Add shop viewmodels

---
+++ WebApplication/Models/ShopItemCategoryViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    public class ShopItemCategoryViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Имя категории")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Описание")]
+        [DataType(DataType.Text)]
+        public string Description { get; set; }
+    }
+}
---
+++ WebApplication/Models/ShopItemViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    public class ShopItemViewModel
+    {
+        public string GameId { get; set; }
+
+        [Display(Name = "Наименование")]
+        [Required(ErrorMessage = "Обязательное поле")]
+        [MinLength(3, ErrorMessage = "Минимальная длина - 3 символа")]
+        public string Name { get; set; }
+
+        [Display(Name = "Описание")]
+        [Required(ErrorMessage = "Обязательное поле")]
+        [DataType(DataType.Text)]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Обязательное поле")]
+        [Display(Name = "Категория")]
+        public int Category { get; set; }
+
+        [Required(ErrorMessage = "Обязательное поле")]
+        [Display(Name = "Изображение")]
+        public byte[] Image { get; set; }
+
+        [Required]
+        [Display(Name = "Цена")]
+        [Range(0.01f, float.MaxValue, ErrorMessage = "Минимальное значение цены 0.01 рубля")]
+        public float Price { get; set; }
+    }
+}
