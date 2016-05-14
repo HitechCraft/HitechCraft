49eecb002bc15e83ca47c5c89eb61990fbd16d6e
HitechCraft <hitechcraft.ru@gmail.com>
2016-05-14 23:51:11 +0300

    Add PlayerSession Model

---
+++ WebApplication/Domain/PlayerSession.cs
@@ -0,0 +1,20 @@
+﻿namespace WebApplication.Domain
+{
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class PlayerSession
+    {
+        [ForeignKey("Player")]
+        public string PlayerName { get; set; }
+
+        public string Session { get; set; }
+
+        public string Server { get; set; }
+
+        public string Token { get; set; }
+
+        public string Md5 { get; set; }
+
+        public virtual Player Player { get; set; }
+    }
+}
