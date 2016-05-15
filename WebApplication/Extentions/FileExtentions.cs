7f59ca2ae61e1bbc575d56cbceec341fc5c6733f
HitechCraft <hitechcraft.ru@gmail.com>
2016-05-15 15:19:58 +0300

    Add file extentions

---
+++ WebApplication/Extentions/FileExtentions.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication.Extentions
+{
+    using System.IO;
+
+    public static class FileExtentions
+    {
+        public static bool IsDirOrFileExists(string dir)
+        {
+            return File.Exists(dir);
+        }
+    }
+}
