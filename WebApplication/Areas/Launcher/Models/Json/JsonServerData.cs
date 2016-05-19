339031b68c81ac6a8b6c42a4b7cdf58d58552aa4
Random <denis-borisov2@rambler.ru>
2016-05-19 19:13:20 +0300

    Add controllers

---
+++ WebApplication/Areas/Launcher/Models/Json/JsonServerData.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication.Areas.Launcher.Models.Json
+{
+    public class JsonServerData
+    {
+        public string Hostname { get; set; }
+
+        public int PlayerCount { get; set; }
+
+        public int MaxPlayerCount { get; set; }
+
+        public int Protocol { get; set; }
+
+        public string Version { get; set; }
+    }
+}
---
+++ WebApplication/Controllers/ServerController.cs
@@ -0,0 +1,20 @@
+﻿namespace WebApplication.Controllers
+{
+    using Areas.Launcher.Models.Json;
+    using Domain;
+    using System.Web.Mvc;
+
+    public class ServerController : BaseController
+    {
+        public JsonResult GetServerData(string ipAddress, int port)
+        {
+            var serverData = new Server()
+            {
+                IpAddress = ipAddress,
+                Port = port
+            }.GetServerData();
+
+            return Json(new JsonServerData());
+        }
+    }
+}
