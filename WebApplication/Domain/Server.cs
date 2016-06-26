287c0ec38b2b2e4104530a8906c46763a455a707
Random <hitechcraft.ru@gmail.com>
2016-06-27 00:41:00 +0300

    Upd server domain

--- WebApplication/Domain/Server.cs
+++ WebApplication/Domain/Server.cs
@@ -8,21 +8,42 @@
     using System.Web.Mvc;
     #endregion
 
-    [Bind(Include= "Id, Name, Description, ClientVersion, IpAddress, Port, ServerModifications")]
+    [Bind(Include= "Id, Name, Description, ClientVersion, IpAddress, Port, MapPort, ServerModifications")]
     public class Server
     {
+        /// <summary>
+        /// Server Id
+        /// </summary>
         [Key]
         public int Id { get; set; }
-
+        /// <summary>
+        /// Server name
+        /// </summary>
         public string Name { get; set; }
-
+        /// <summary>
+        /// Server description
+        /// </summary>
         public string Description { get; set; }
-
+        /// <summary>
+        /// Server image
+        /// </summary>
+        public byte[] Image { get; set; }
+        /// <summary>
+        /// Client version for cur server
+        /// </summary>
         public string ClientVersion { get; set; }
-
+        /// <summary>
+        /// Server adress
+        /// </summary>
         public string IpAddress { get; set; }
-
+        /// <summary>
+        /// Server port
+        /// </summary>
         public int Port { get; set; }
+        /// <summary>
+        /// For Dynmap
+        /// </summary>
+        public int MapPort { get; set; }
 
         public ISet<ServerModification> ServerModifications { get; set; }
     }
