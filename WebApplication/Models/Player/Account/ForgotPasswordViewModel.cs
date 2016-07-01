acf8adb6ba05e412e20eadafb97ebddf63c746e1
Random <hitechcraft.ru@gmail.com>
2016-07-01 16:05:20 +0300

    Refactoring

---
+++ WebApplication/Models/Player/Account/ForgotPasswordViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class ForgotPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Почта")]
+        public string Email { get; set; }
+    }
+}
