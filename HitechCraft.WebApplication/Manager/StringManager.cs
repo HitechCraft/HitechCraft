cd1997859d5259f780eb8de41fd4339b89556829
Random <denis-borisov2@rambler.ru>
2016-08-03 13:59:00 +0300

    Add  string extention to right cutting limitted string

---
+++ HitechCraft.WebApplication/Manager/StringManager.cs
@@ -0,0 +1,26 @@
+﻿namespace HitechCraft.DAL.Domain.Extentions
+{
+    #region Using Directives
+
+    using System;
+    using System.Linq;
+
+    #endregion
+
+    public static class StringManager
+    {
+        public static string Limit(this string str, int stringLength)
+        {
+            var words = str.Split(' ').ToList();
+            var newString = String.Empty;
+
+            foreach (var word in words)
+            {
+                if (newString.Length < stringLength) newString += word + " ";
+                else return newString;
+            }
+
+            return newString;
+        }
+    }
+}
