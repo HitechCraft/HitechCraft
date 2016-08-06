b611529d5898ff91c09250b08335f2575cc99034
Random <hitechcraft.ru@gmail.com>
2016-08-06 03:43:10 +0300

    Add  skin catalog domains

---
+++ HitechCraft.DAL/Domain/Minecraft/Skin.cs
@@ -0,0 +1,11 @@
+﻿namespace HitechCraft.DAL.Domain
+{
+    using Common.Entity;
+
+    public class Skin : BaseEntity<Skin>
+    {
+        public string Name { get; set; }
+
+        public byte[] Image { get; set; }
+    }
+}
