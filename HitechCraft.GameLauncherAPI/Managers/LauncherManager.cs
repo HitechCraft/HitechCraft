832e8ad24e9662705a8d0ec8abee7b4061aa381c
Random <denis-borisov2@rambler.ru>
2016-08-21 10:20:32 +0300

    Add  launcher api project

---
+++ HitechCraft.GameLauncherAPI/Managers/LauncherManager.cs
@@ -0,0 +1,30 @@
+﻿namespace HitechCraft.GameLauncherAPI.Managers
+{
+    #region Using Directives
+
+    using System.Collections.Generic;
+
+    #endregion
+
+    public static class LauncherManager
+    {
+        #region Methods
+
+        public static List<string> GetRequiredFolderList(string clientName)
+        {
+            var clientDir = LauncherConfig.ClientsDir + "/" + clientName + "/";
+
+            return new List<string>
+            {
+                LauncherConfig.ClientsDir + "/" + "assets.zip",
+                clientDir + "bin" + "/",
+                clientDir + "natives" + "/",
+                clientDir + "mods" + "/",
+                clientDir + "coremods" + "/",
+                clientDir + "config.zip"
+            };
+        }
+
+        #endregion
+    }
+}
