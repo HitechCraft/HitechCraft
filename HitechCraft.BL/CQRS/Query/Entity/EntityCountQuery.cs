4ac01987e888eaf43ce9480c738de462288e624f
Random <denis-borisov2@rambler.ru>
2016-09-18 13:28:13 +0200

    Add  entity counter

---
+++ HitechCraft.BL/CQRS/Query/Entity/EntityCountQuery.cs
@@ -0,0 +1,14 @@
+﻿namespace HitechCraft.BL.CQRS.Query
+{
+    #region Using Directives
+
+    using HitechCraft.Core.Entity.Base;
+    using HitechCraft.Core.Repository.Specification;
+
+    #endregion
+
+    public class EntityCountQuery<TEntity> : IQuery<int> where TEntity : BaseEntity<TEntity>
+    {
+        public ISpecification<TEntity> Specification { get; set; }
+    }
+}
