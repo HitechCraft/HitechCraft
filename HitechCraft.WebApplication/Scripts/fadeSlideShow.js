7a748f42ae8e32fdf2999c9b6d9a8d6eda37a902
Random <hitechcraft.ru@gmail.com>
2016-12-11 14:35:33 +0300

    New Design  Fix slider text

--- HitechCraft.WebApplication/Scripts/fadeSlideShow.js
+++ HitechCraft.WebApplication/Scripts/fadeSlideShow.js
@@ -64,7 +64,10 @@ jQuery.fn.fadeSlideShow = function(options) {
 					setActLi = (Slides - ActSlide) + 1;
 					if(setActLi > Slides){setActLi=0;}
 					jQuery('#'+settings.ListElement+' li').removeClass(settings.ListLiActive);
-					jQuery('#'+settings.ListElement+' li').eq(setActLi).addClass(settings.ListLiActive);
+					jQuery('#' + settings.ListElement + ' li').eq(setActLi).addClass(settings.ListLiActive);
+
+					$('.title-text').text($(jQslide[setActLi]).find('.text').text());
+					$('.title-desc').text($(jQslide[setActLi]).find('.desc').text());
 				}
 				
 				if(ActSlide <= 0){
@@ -79,112 +82,6 @@ jQuery.fn.fadeSlideShow = function(options) {
 				jQuery('#'+settings.PlayPauseElement).html(settings.PauseText);
 			}
 		}
-		
-		stopAutoplay = function(){
-			clearInterval(intval);
-			intval = false;
-			if(settings.PlayPauseElement){
-				jQuery('#'+settings.PlayPauseElement).html(settings.PlayText);
-			}
-		}
-		
-		jumpTo = function(newIndex){
-			if(newIndex < 0){newIndex = Slides;}
-			else if(newIndex > Slides){newIndex = 0;}
-			if( newIndex >= ActSlide ){
-				jQuery('> *:lt('+(newIndex+1)+')', fssThis).fadeIn(settings.speed);
-			}else if(newIndex <= ActSlide){
-				jQuery('> *:gt('+newIndex+')', fssThis).fadeOut(settings.speed);
-			}
-			
-			// set the active slide
-			ActSlide = newIndex;
-
-			if(settings.ListElement){
-				// set active
-				jQuery('#'+settings.ListElement+' li').removeClass(settings.ListLiActive);
-				jQuery('#'+settings.ListElement+' li').eq((Slides-newIndex)).addClass(settings.ListLiActive);
-			}
-		}
-		
-		// if list is on render it
-		// if(settings.ListElement){
-			// i=0;
-			// li = '';
-			// while(i<=Slides){
-				// if(i==0){
-					// li = li+'<li class="'+settings.ListLi+i+' '+settings.ListLiActive+'"><a href="#">'+(i+1)+'<\/a><\/li>';
-				// }else{
-					// li = li+'<li class="'+settings.ListLi+i+'"><a href="#">'+(i+1)+'<\/a><\/li>';
-				// }
-				// i++;
-			// }
-			// List = '<ul id="'+settings.ListElement+'">'+li+'<\/ul>';
-			
-			// // add list to a special id or append after the slideshow
-			// if(settings.addListToId){
-				// jQuery('#'+settings.addListToId).append(List);
-			// }else{
-				// jQuery(this).after(List);
-			// }
-			
-			// jQuery('#'+settings.ListElement+' a').bind('click', function(){
-				// index = jQuery('#'+settings.ListElement+' a').index(this);
-				// stopAutoplay();
-				// ReverseIndex = Slides-index;
-				
-				// jumpTo(ReverseIndex);
-				
-				// return false;
-			// });
-		// }
-		
-		// if(settings.PlayPauseElement){
-			// if(!jQuery('#'+settings.PlayPauseElement).css('display')){
-				// jQuery(this).after('<a href="#" id="'+settings.PlayPauseElement+'"><\/a>');
-			// }
-			
-			// if(settings.autoplay){
-				// jQuery('#'+settings.PlayPauseElement).html(settings.PauseText);
-			// }else{
-				// jQuery('#'+settings.PlayPauseElement).html(settings.PlayText);
-			// }
-			
-			// jQuery('#'+settings.PlayPauseElement).bind('click', function(){
-				// if(intval){
-					// stopAutoplay();
-				// }else{
-					// autoplay();
-				// }
-				// return false;
-			// });
-		// }
-		
-		// if(settings.NextElement){
-			// if(!jQuery('#'+settings.NextElement).css('display')){
-				// jQuery(this).after('<a href="#" id="'+settings.NextElement+'">'+settings.NextElementText+'<\/a>');
-			// }
-			
-			// jQuery('#'+settings.NextElement).bind('click', function(){
-				// nextSlide = ActSlide-1;
-				// stopAutoplay();
-				// jumpTo(nextSlide);
-				// return false;
-			// });
-		// }
-		
-		// if(settings.PrevElement){
-			// if(!jQuery('#'+settings.PrevElement).css('display')){
-				// jQuery(this).after('<a href="#" id="'+settings.PrevElement+'">'+settings.PrevElementText+'<\/a>');
-			// }
-			
-			// jQuery('#'+settings.PrevElement).bind('click', function(){
-				// prevSlide = ActSlide+1;
-				// stopAutoplay();
-				// jumpTo(prevSlide);
-				// return false;
-			// });
-		// }
 		
 		if(settings.allowKeyboardCtrl){
 			jQuery(document).bind('keydown', function(e){
